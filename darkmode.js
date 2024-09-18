2795e7bcc97f51d12a5a1ba4848fd8370ed111be
Emil Begovic <emil@begovic.se>
2024-09-18 14:00:52 +0200

    Added javascript for darkmode toggle

---
+++ darkmode.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleSwitch = document.querySelector(
+    '#darkmode-switch input[type="checkbox"]'
+  );
+  const rootElement = document.documentElement;
+
+  // Function to switch themes
+  function switchTheme(e) {
+    if (e.target.checked) {
+      rootElement.setAttribute("data-bs-theme", "dark");
+      localStorage.setItem("theme", "dark"); // Save the theme choice
+    } else {
+      rootElement.setAttribute("data-bs-theme", "light");
+      localStorage.setItem("theme", "light"); // Save the theme choice
+    }
+  }
+
+  // Add event listener to the toggle switch
+  toggleSwitch.addEventListener("change", switchTheme);
+
+  // Check the saved theme on page load
+  const currentTheme = localStorage.getItem("theme") || "light";
+  if (currentTheme === "dark") {
+    toggleSwitch.checked = true;
+    rootElement.setAttribute("data-bs-theme", "dark");
+  } else {
+    toggleSwitch.checked = false;
+    rootElement.setAttribute("data-bs-theme", "light");
+  }
+});
